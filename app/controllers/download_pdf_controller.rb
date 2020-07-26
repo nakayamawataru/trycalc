9a83fc3074fb7a6abf2fe2208dc9a1a466c61c6a
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2020-07-26 13:14:21 +0000

    リファクタリング

--- app/controllers/download_pdf_controller.rb
+++ app/controllers/download_pdf_controller.rb
@@ -1,7 +1,8 @@
 class DownloadPdfController < ActionController::Base
+    before_action :set_plan,  only: [:meocheki_quotation, :hoshikakutokun_quotation]
+    before_action:set_customer
     
     def meocheki_quotation
-        @plan = Plan.find_by(month_price: params[:plans])
         @add_time_count = params[:crawl_time].to_i
         @add_loc_count = params[:crawl_loc].to_i
         
@@ -27,11 +28,9 @@ class DownloadPdfController < ActionController::Base
         @add_loc_price = @add_loc_count * 300 * @pran_period
         
         @plan_name = @plan.name
-        @business_name = params[:business_name]
         @total_price = @plan_price + @add_time_price + @add_loc_price
         @tax = (@total_price * 0.1).to_i
         @in_tax_price = @total_price + @tax
-        @email = params[:email]
 
         pdf_file = generate_pdf_file
             
@@ -63,18 +62,22 @@ class DownloadPdfController < ActionController::Base
         @price_model = RentalPlanPrice.find_by(price:params[:plans])
         @plan_price = params[:plans].to_i
         
+        @post_price = 0
+        @auto_reply_reviews_price = 0
+        @report_price = 0 
+        
         if  @plan_price < 140000
-            params[:post].present? ? @post_price = 25000 : 0
-            params[:auto_reply_reviews].present? ? @auto_reply_reviews_price = 20000 : 0
-            params[:report].present? ? @report_price = 25000 : 0 
+            @post_price               = 25000 if params[:post].present?
+            @auto_reply_reviews_price = 20000 if params[:auto_reply_reviews].present?
+            @report_price             = 25000 if params[:report].present?
         elsif 140000 <= @plan_price &&  @plan_price <= 230000
-            params[:post].present? ? @post_price = 50000 : 0
-            params[:auto_reply_reviews].present? ? @auto_reply_reviews_price = 40000 : 0
-            params[:report].present? ? @report_price = 50000 : 0 
+            @post_price               = 50000 if params[:post].present?
+            @auto_reply_reviews_price = 40000 if params[:auto_reply_reviews].present?
+            @report_price             = 50000 if params[:report].present?
         else
-            params[:post].present? ? @post_price = 75000 : 0
-            params[:auto_reply_reviews].present? ? @auto_reply_reviews_price = 55000 : 0
-            params[:report].present? ? @report_price = 75000 : 0 
+            @post_price               = 75000 if params[:post].present?
+            @auto_reply_reviews_price = 55000 if params[:auto_reply_reviews].present?
+            @report_price             = 75000 if params[:report].present?
         end
         
         if params[:license].present?
@@ -83,14 +86,12 @@ class DownloadPdfController < ActionController::Base
         
         
         @pran_period = 12
-        @business_name = params[:business_name]
         @total_price = @plan_price + @post_price.to_i + @auto_reply_reviews_price.to_i + @report_price.to_i + @license_price.to_i
         @in_init_price = @total_price + @init_price.to_i
         @tax = (@total_price * 0.1).to_i
         @in_tax_price = @total_price + @tax
         @in_init_tax = (@in_init_price * 0.1).to_i
         @in_init_tax_price = @in_init_price + @in_init_tax
-        @email = params[:email]
 
         
         pdf_file = render_to_string pdf: '',
@@ -132,18 +133,11 @@ class DownloadPdfController < ActionController::Base
         end
             
         @plan_name = @plan.name
-        @business_name = params[:business_name]
         @total_price = @plan_price
         @tax = (@total_price * 0.1).to_i
         @in_tax_price = @total_price + @tax
-        @email = params[:email]
 
-        pdf_file = render_to_string pdf: '',
-            template: 'pdf/quotation',
-            encoding: 'UTF-8',
-            layout: 'pdf',
-            #format: :html
-            page_size: 'A4'
+        pdf_file = generate_pdf_file
             
         # render pdf: 'file_name', #pdfファイルの名前。これがないとエラーが出ます
         #       layout: 'pdf', #レイアウトファイルの指定。views/layoutsが読まれます。
@@ -166,50 +160,41 @@ class DownloadPdfController < ActionController::Base
     end
     
     def hoshikakutokun_quotation
-        @plan = Plan.find_by(month_price: params[:plans])
-        @add_time_count = (params[:crawl_time].present? ? params[:crawl_time] : 0).to_i
-        @add_loc_count = (params[:crawl_loc].present? ? params[:crawl_loc] : 0).to_i
-        @message_count = (params[:message_count].present? ? params[:message_count] : 0).to_i
+        @add_time_count = (params[:crawl_time] || 0).to_i
+        @add_loc_count = (params[:crawl_loc] || 0).to_i
+        @message_count = (params[:message_count]&.to_i || 0).to_i
         @message_price = @message_count / 50 * 1000
 
         if params[:credit_calc].present?
             @payment_type = params[:payment_type]
-            if @payment_type == "credit_discount_month_price"
+            
+            case @payment_type
+            when "credit_discount_month_price"
                 @plan_price = @plan.credit_discount_month_price.to_i
                 @pran_period = 1
-                @add_time_price = @add_time_count * 150 * @pran_period
-                @add_loc_price = @add_loc_count * 300 * @pran_period
-            elsif @payment_type == "price_half_year"
+            when "price_half_year"
                 @plan_price = @plan.price_half_year.to_i
                 @pran_period = 6
-                @add_time_price = @add_time_count * 150 * @pran_period
-                @add_loc_price = @add_loc_count * 300 * @pran_period
-            elsif @payment_type == "price_one_year"
+            when "price_one_year"
                 @plan_price = @plan.price_one_year.to_i
                 @pran_period = 12
-                @add_time_price = @add_time_count * 150 * @pran_period
-                @add_loc_price = @add_loc_count * 300 * @pran_period
+            else
+                raise "unknown payment type"
             end
+            
         else
             @plan_price = @plan.month_price
             @pran_period = 1
-            @add_time_price = @add_time_count * 150
-            @add_loc_price = @add_loc_count * 300
         end
 
+        @add_time_price = @add_time_count * 150 * @pran_period
+        @add_loc_price = @add_loc_count * 300 * @pran_period
         @plan_name = @plan.name
-        @business_name = params[:business_name]
         @total_price = @plan_price.to_i + @add_time_price.to_i + @add_loc_price.to_i + @message_price.to_i
         @tax = (@total_price * 0.1).to_i
         @in_tax_price = @total_price + @tax
-        @email = params[:email]
 
-        pdf_file = render_to_string pdf: '',
-            template: 'pdf/quotation',
-            encoding: 'UTF-8',
-            layout: 'pdf',
-            #format: :html
-            page_size: 'A4'
+        pdf_file = generate_pdf_file
             
         # render pdf: 'file_name', #pdfファイルの名前。これがないとエラーが出ます
         #       layout: 'pdf', #レイアウトファイルの指定。views/layoutsが読まれます。
@@ -238,6 +223,15 @@ class DownloadPdfController < ActionController::Base
             layout: 'pdf',
             #format: :html
             page_size: 'A4'
+    end
+    
+    def set_plan
+        @plan = Plan.find_by(month_price: params[:plans])
+    end
+    
+    def set_customer
+        @business_name = params[:business_name]
+        @email = params[:email]
     end
 
 end
