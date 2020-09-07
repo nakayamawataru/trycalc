0d8b2cf5065054daa3c70f5abb46c5725b546c5c
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2020-09-07 09:17:12 +0000

    allow all iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_action :set_cors_headers
+  
+  private
+
+  def set_cors_headers
+    response.set_header 'Access-Control-Allow-Origin', origin
+    response.set_header 'X-Frame-Options', 'ALLOWALL'
+  end
+
+  def origin
+    request.headers["Origin"] || "*"
+  end
 end
