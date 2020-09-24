b53c366ffc34843f04f5c2ceaa9fcb11d09863f6
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2020-09-24 08:47:40 +0000

    gonがない場合はチャート不発に

--- app/assets/javascripts/components/multiple_stores.js
+++ app/assets/javascripts/components/multiple_stores.js
@@ -2,98 +2,100 @@
 /*global gon*/
 /*global Chart*/
 $(document).on('turbolinks:load', function() {
-    
-    var costData = {
-        labels: ["現在のコスト", "導入後のコスト"],
-        datasets: [{
-            label: 'コスト',
-            backgroundColor: "#7BD0DB",
-            borderColor: "#7BD0DB",
-            borderWidth: 1,
-            data: gon.cost
-        }]
-    };
-    
-    var costConfig = {
-        type: 'bar',
-        data: costData,
-        options: {
-            responsive: false,
-            legend: {
-                display: false,
-            },
-            title: {
-                display: false
-            },
-            tooltips: {
-                callbacks: {
-                    label: function(tooltipItem, data){
-                        return tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') +' 円';
-                    },
+    var gonData = typeof gon;
+    if (gonData != 'undefined'){
+        var costData = {
+            labels: ["現在のコスト", "導入後のコスト"],
+            datasets: [{
+                label: 'コスト',
+                backgroundColor: "#7BD0DB",
+                borderColor: "#7BD0DB",
+                borderWidth: 1,
+                data: gon.cost
+            }]
+        };
+        
+        var costConfig = {
+            type: 'bar',
+            data: costData,
+            options: {
+                responsive: false,
+                legend: {
+                    display: false,
+                },
+                title: {
+                    display: false
+                },
+                tooltips: {
+                    callbacks: {
+                        label: function(tooltipItem, data){
+                            return tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') +' 円';
+                        },
+                        mode: 'index',
+                        intersect: false
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 0,
+                            callback: function(value) {return '¥' + value.toLocaleString();}
+                        }
+                    }]
+                },
+                animation: {
+                    animateRotate: true
+                }
+            }
+        };
+        
+        var timeCostData = {
+            labels: ["現在の所要時間", "導入後の所要時間"],
+            datasets: [{
+                label: '時間',
+                backgroundColor: "#7BD0DB",
+                borderColor: "#7BD0DB",
+                borderWidth: 1,
+                data: gon.time_cost
+            }]
+        };
+        
+        var timeCostConfig = {
+            type: 'bar',
+            data: timeCostData,
+            options: {
+                responsive: false,
+                legend: {
+                    display: false,
+                },
+                title: {
+                    display: false
+                },
+                tooltips: {
                     mode: 'index',
                     intersect: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 0,
+                            callback: function(value) {return value + "時間"}
+                        }
+                    }]
+                },
+                animation: {
+                    animateRotate: true
                 }
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        min: 0,
-                        callback: function(value) {return '¥' + value.toLocaleString();}
-                    }
-                }]
-            },
-            animation: {
-                animateRotate: true
             }
-        }
-    };
-    
-    var timeCostData = {
-        labels: ["現在の所要時間", "導入後の所要時間"],
-        datasets: [{
-            label: '時間',
-            backgroundColor: "#7BD0DB",
-            borderColor: "#7BD0DB",
-            borderWidth: 1,
-            data: gon.time_cost
-        }]
-    };
-    
-    var timeCostConfig = {
-        type: 'bar',
-        data: timeCostData,
-        options: {
-            responsive: false,
-            legend: {
-                display: false,
-            },
-            title: {
-                display: false
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        min: 0,
-                        callback: function(value) {return value + "時間"}
-                    }
-                }]
-            },
-            animation: {
-                animateRotate: true
-            }
-        }
-    };
-    
-    window.onload = function () {
-        var ctx = document.getElementById("cost-chart").getContext("2d");
-        window.bar = new Chart(ctx, costConfig);
-        var ctx = document.getElementById("time-cost-chart").getContext("2d");
-        window.bar = new Chart(ctx, timeCostConfig);
-    };
+        };
+        
+        window.onload = function () {
+            var ctx = document.getElementById("cost-chart").getContext("2d");
+            window.bar = new Chart(ctx, costConfig);
+            var ctx = document.getElementById("time-cost-chart").getContext("2d");
+            window.bar = new Chart(ctx, timeCostConfig);
+        };
+    }
 });
--- app/assets/javascripts/components/pdf_chart.js
+++ app/assets/javascripts/components/pdf_chart.js
@@ -6,81 +6,84 @@
 /*global gon*/
 
 $(document).ready(function () {
-
-    var costData = {
-        labels: ["現在のコスト", "導入後のコスト"],
-        datasets: [{
-            label: 'コスト',
-            backgroundColor: "#7BD0DB",
-            borderColor: "#7BD0DB",
-            borderWidth: 1,
-            data: gon.cost
-        }]
-    };
     
-    var costConfig = {
-        type: 'bar',
-        data: costData,
-        options: {
-            responsive: false,
-            legend: {
-                display: false,
-            },
-            title: {
-                display: false
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        min: 0,
-                        callback: function(value) {return '¥' + value.toLocaleString();}
-                    }
-                }]
-            },
-            animation: false
-        }
-    };
-    
-    var timeCostData = {
-        labels: ["現在の所要時間", "導入後の所要時間"],
-        datasets: [{
-            label: '時間',
-            backgroundColor: "#7BD0DB",
-            borderColor: "#7BD0DB",
-            borderWidth: 1,
-            data: gon.time_cost
-        }]
-    };
-    
-    var timeCostConfig = {
-        type: 'bar',
-        data: timeCostData,
-        options: {
-            responsive: false,
-            legend: {
-                display: false,
-            },
-            title: {
-                display: false
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        min: 0,
-                        callback: function(value) {return value + "時間"}
-                    }
-                }]
-            },
-            animation: false
-        }
-    };
-    
-    var ctx = document.getElementById("cost-chart-pdf").getContext("2d");
-    window.bar = new Chart(ctx, costConfig);
-    var ctx = document.getElementById("time-chart-pdf").getContext("2d");
-    window.bar = new Chart(ctx, timeCostConfig);
+    var gonData = typeof gon;
+    if (gonData != 'undefined'){
+        var costData = {
+            labels: ["現在のコスト", "導入後のコスト"],
+            datasets: [{
+                label: 'コスト',
+                backgroundColor: "#7BD0DB",
+                borderColor: "#7BD0DB",
+                borderWidth: 1,
+                data: gon.cost
+            }]
+        };
+        
+        var costConfig = {
+            type: 'bar',
+            data: costData,
+            options: {
+                responsive: false,
+                legend: {
+                    display: false,
+                },
+                title: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 0,
+                            callback: function(value) {return '¥' + value.toLocaleString();}
+                        }
+                    }]
+                },
+                animation: false
+            }
+        };
+        
+        var timeCostData = {
+            labels: ["現在の所要時間", "導入後の所要時間"],
+            datasets: [{
+                label: '時間',
+                backgroundColor: "#7BD0DB",
+                borderColor: "#7BD0DB",
+                borderWidth: 1,
+                data: gon.time_cost
+            }]
+        };
+        
+        var timeCostConfig = {
+            type: 'bar',
+            data: timeCostData,
+            options: {
+                responsive: false,
+                legend: {
+                    display: false,
+                },
+                title: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 0,
+                            callback: function(value) {return value + "時間"}
+                        }
+                    }]
+                },
+                animation: false
+            }
+        };
+        
+        var ctx = document.getElementById("cost-chart-pdf").getContext("2d");
+        window.bar = new Chart(ctx, costConfig);
+        var ctx = document.getElementById("time-chart-pdf").getContext("2d");
+        window.bar = new Chart(ctx, timeCostConfig);
+    }
 
 });
 
