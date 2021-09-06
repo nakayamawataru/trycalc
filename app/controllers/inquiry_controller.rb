aace80072a671e39ac1a6009054a46d96b5e0254
NobuhideYoshioka <n.yoshioka@tryhatch.co.jp>
2021-09-06 14:07:45 +0900

    申込み・問い合わせフォームのベースを作成

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,4 +1,20 @@
 class InquiryController < ApplicationController
   def new
+    @inquiry = Inquiry.new
   end
+
+  def create
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.save
+      InquiryMailer.inquiry_mail(@inquiry).deliver
+      # サンキューページを新規作成or改造して、そちらに飛ばす
+      redirect_to thanx_page_path
+    else
+      # 画面遷移せずにJSでエラーの表示をするようにする
+      render 
+    end
+    
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   
   resources :plans do
   end
+  
+  resources :inquiries
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
